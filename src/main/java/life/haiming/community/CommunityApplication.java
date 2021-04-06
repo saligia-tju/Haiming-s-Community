cb423c1c32545bbf1a0c0a0c992b6a5f3294b7d8
saligia-tju <hmyang_tju@163.com>
2021-04-07 01:32:27 +0800

    add sql ddl

--- src/main/java/life/haiming/community/CommunityApplication.java
+++ src/main/java/life/haiming/community/CommunityApplication.java
@@ -2,8 +2,10 @@ package life.haiming.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"life.haiming.community.mapper"})
 public class CommunityApplication {
 
     public static void main(String[] args) {
